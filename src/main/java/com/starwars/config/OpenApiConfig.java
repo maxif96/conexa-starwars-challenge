eec48f675fbf1fb4661fbe43f7cc8ad4b1926565
Maxi Falcone <maxifop96@gmail.com>
2025-08-15 22:06:14 -0300

    Enriqueciendo configuración OpenAPI

--- src/main/java/com/starwars/config/OpenApiConfig.java
+++ src/main/java/com/starwars/config/OpenApiConfig.java
@@ -5,30 +5,29 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${server.servlet.context-path:/}")
-    private String contextPath;
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .servers(Arrays.asList(new Server().url(contextPath)))
+                .servers(Arrays.asList(
+                        new Server().url("https://conexa-starwars-api-f5c72652ce2f.herokuapp.com").description("Heroku Production"),
+                        new Server().url("http://localhost:8080").description("Local Development")
+                ))
                 .info(new Info()
                         .title("Star Wars API")
-                        .description("API to consume and serve Star Wars data from swapi.tech")
+                        .description("API de Star Wars con autenticación JWT - Challenge Técnico Conexa")
                         .version("1.0.0")
                         .contact(new Contact()
-                                .name("Star Wars API Team")
-                                .email("contact@starwarsapi.com"))
+                                .name("Maxif96")
+                                .email("maxifop96@gmail.com")
+                                .url("https://github.com/maxif96"))
                         .license(new License()
                                 .name("MIT License")
                                 .url("https://opensource.org/licenses/MIT")));
